faf6f6dc6aeb5fb735c6e8d6e6209e3dacc9622f
David Silva <david.silva@seriedesign.com.br>
2022-06-21 13:52:02 -0300

    Aplicação base

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
+import "../styles/repositories.scss";
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
+      <ul>
+
+        {repositories.map((repo) => (
+          <RepositoryItem key={repo.name} repository={repo} />
+        ))}
+      </ul>
+    </section>
+  );
+}
